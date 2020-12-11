e8f913026c2fb2e9434e939eeeb8c2431731afc4
suhaas <footballandgermany@gmail.com>
2020-12-11 20:03:53 +0530

    header and menu-item

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,52 @@
+
+import React, { useContext, useState } from 'react';
+
+
+// import { auth } from '../../firebase/firebase.utils';
+// import CartIcon from '../cart-icon/cart-icon.component';
+// import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+// import CurrentUserContext from '../../contexts/current-user/current-user.context';
+// import { CartContext } from '../../providers/cart/cart.provider';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink
+} from './header.styles';
+
+const Header = () => {
+  const currentUser = useContext(CurrentUserContext);
+  const { hidden } = useContext(CartContext);
+
+  return (
+    <HeaderContainer>
+      <LogoContainer to='/'>
+        <Logo className='logo' />
+      </LogoContainer>
+      < OptionsContainer>
+        < OptionLink  to='/shop'>
+          SHOP
+        </OptionLink>
+        <OptionLink to='/shop'>
+          CONTACT
+        </OptionLink>
+        {/* {currentUser ? (
+          <OptionLink onClick={() => auth.signOut()}>
+            SIGN OUT
+          </OptionLink>
+        ) : (
+          <OptionLink to='/signin'>
+            SIGN IN
+          </OptionLink>
+        )} */}
+        {/* <CartIcon /> */}
+      </OptionsContainer>
+      {/* {hidden ? null : <CartDropdown />} */}
+    </HeaderContainer>
+  );
+};
+
+export default Header;
